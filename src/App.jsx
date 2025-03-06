9c0c645f295c8d9a17cf5a127928ea12eea0c1e0
Miguel Eduardo Gonzalez Fofe <247mmgg@gmail.com>
2025-03-05 23:49:13 -0300

    se agrego el itemListContainer con las props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import Header from './components/Header/header_n'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 
 function App() {
 
   return (
     <>
       <Header />
-
+      <ItemListContainer saludo={"Hola, este Sera un contenedor para los productos"} />
     </>
   )
 }
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+
+const temListContainer = ({saludo}) => {
+
+    return<>
+        <h1 className="text-center">{saludo}</h1>
+    </>
+
+}
+
+export default temListContainer;
